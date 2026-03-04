c2dd2c4441019ead05fecf8a7958f6d852086f3f
kirtick28 <kirtick.mm2023cse@sece.ac.in>
2026-03-04 11:31:31 +0530

    patch student.test file

--- tests/student.test.js
+++ tests/student.test.js
@@ -1,4 +1,4 @@
-*import request from 'supertest';
+import request from 'supertest';
 import app from '../app.js';
 import Student from '../models/Student.js';
 import Department from '../models/Department.js';

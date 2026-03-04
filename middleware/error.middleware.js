cfaf60cea4f2817f9038a3ebe412e60ad53882b9
kirtick28 <kirtick.mm2023cse@sece.ac.in>
2026-03-04 09:14:56 +0530

    Updated Global Error handler

--- middleware/error.middleware.js
+++ middleware/error.middleware.js
@@ -1,20 +1,38 @@
-const globalErrorHandler = (err, req, res, next) => {
-  console.error('ERROR 💥:', err);
+import AppError from '../utils/AppError.js';
 
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
 
-  // Development response
-  if (process.env.NODE_ENV === 'development') {
-    return res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
-      stack: err.stack,
-      error: err
-    });
-  }
+const handleDuplicateFieldsDB = (err) => {
+  const value = Object.values(err.keyValue)[0];
+  const message = `Duplicate field value: ${value}. Please use another value`;
+  return new AppError(message, 400);
+};
 
-  // Production response
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
+const handleJWTError = () =>
+  new AppError('Invalid token. Please log in again!', 401);
+
+const handleJWTExpiredError = () =>
+  new AppError('Your token has expired! Please log in again.', 401);
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
   if (err.isOperational) {
     return res.status(err.statusCode).json({
       status: err.status,
@@ -22,11 +40,33 @@ const globalErrorHandler = (err, req, res, next) => {
     });
   }
 
-  // Unknown error
-  return res.status(500).json({
+  console.error('ERROR 💥', err);
+
+  res.status(500).json({
     status: 'error',
-    message: 'Something went wrong'
+    message: 'Something went wrong!'
   });
+};
+
+const globalErrorHandler = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+    error.message = err.message;
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
+    if (error.name === 'JsonWebTokenError') error = handleJWTError();
+    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
+
+    sendErrorProd(error, res);
+  }
 };
 
 export default globalErrorHandler;
